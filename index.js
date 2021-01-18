9ce5237735b9d23159a235581417204b8a625b6b
sojeong_kim <sojeong_kim@tmax.co.kr>
2021-01-18 13:24:55 +0900

    create express server

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log('hi')
+const express =require("express"); // express 불러오기
+const app = express(); // application 만들기(express 실행해서 담기)
+
+const PORT = 4000;
+
+// npm start하면 실행
+const handleListening = () => {
+  console.log(`Listening on: http://localhost:${PORT}`);
+}
+app.listen(PORT, handleListening);
