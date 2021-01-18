650e62103f18a52433ff3ac5221a6c2c443da849
sojeong_kim <sojeong_kim@tmax.co.kr>
2021-01-18 20:10:46 +0900

    install express,babel,nodemon

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const express =require("express"); // express 불러오기
+import express from "express"; // express 불러오기
 const app = express(); // application 만들기(express 실행해서 담기)
 
 const PORT = 4000;
@@ -7,4 +7,14 @@ const PORT = 4000;
 const handleListening = () => {
   console.log(`Listening on: http://localhost:${PORT}`);
 }
+
+// reqest object와 response object를 받는다
+const handleHome = (req, res) => res.send("Hello from home~");
+
+const handleProfile = (req,res) => res.send("You are on my Profile");
+
+// create routes
+app.get("/", handleHome);
+app.get("/profile", handleProfile);
+
 app.listen(PORT, handleListening);
